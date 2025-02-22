69de90c03a5bd63fe1744d019c3d6e4af21bcbf1
Ajiboye Adeoye <ajiboyeadeoye25@yahoo.com>
2025-02-22 07:56:09 +0100

    Updated

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignupForm
 
 # Create your views here.
 
@@ -19,3 +21,18 @@ def contact(request):
 
 def test(request):
     return render(request, 'core/test.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
+     
